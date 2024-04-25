db5623001d9c7382fcfb41fe3bad43fa87f541bb
Philippe <you@example.com>
2024-04-25 10:49:53 -0400

    siege1 pas encore fini

--- AvionBiblio/AvionBiblio/Siege.cs
+++ AvionBiblio/AvionBiblio/Siege.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace AvionBiblio
 {
-    internal class Siege
+    public class Siege
     {
+       
+       private enum _categorie
+       {
+            Premiere,
+            Reguliere,
+            Economique
+       }
+        private _categorie _categories;
+        private int _codeSiege;
+
+
+        public Siege(_categorie categories, int codeSiege)
+        {
+            this.Categories = categories;
+           this.CodeSiege = codeSiege;
+        }
+
+
+        private _categorie Categories
+        { 
+            get => _categories;
+            set => _categories = value; 
+        }
+        public int CodeSiege 
+        { 
+            get => _codeSiege;
+            set => _codeSiege = value;
+        }
     }
 }
