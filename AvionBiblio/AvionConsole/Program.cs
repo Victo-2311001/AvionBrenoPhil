a53e68f06dd3ddbe3c63c703ccffe708e5c7a558
Victo-2311001 <143755586+Victo-2311001@users.noreply.github.com>
2024-04-25 10:10:07 -0400

    Add files via upload

---
+++ AvionBiblio/AvionConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AvionConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
